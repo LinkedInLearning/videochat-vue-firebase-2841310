ae662ea4e34b5791caf5835c7fc3db01ad396268
Ray Villalobos <ray@planetoftheweb.com>
2020-08-02 17:07:35 -0700

    customizing our template

---
+++ src/components/Navigation.vue
@@ -0,0 +1,6 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/login">Login</router-link>
+  </div>
+</template>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1>Home</h1>
+    <h1>Login</h1>
   </div>
 </template>
