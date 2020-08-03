a3c24bccb838b707970bd089719b2042807bd1e1
Ray Villalobos <ray@planetoftheweb.com>
2020-08-02 18:32:04 -0700

    Added Firebase data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>
+  <div class="mt-3 container">
     <h1>Home</h1>
+    <p>Welcome {{ user }}</p>
   </div>
 </template>
+<script>
+import db from '../db.js'
+export default {
+  name: 'home',
+  data: function() {
+    return {
+      user: null
+    }
+  },
+  mounted() {
+    db.collection('users')
+      .doc('39mtRZ22vlE7gp3L11Qk')
+      .get()
+      .then(snapshot => {
+        this.user = snapshot.data().name
+      })
+  }
+}
+</script>
