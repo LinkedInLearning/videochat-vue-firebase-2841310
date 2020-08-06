a9fdf59956e8fc2f2901cdde839a139cc69a65d6
Ray Villalobos <ray@planetoftheweb.com>
2020-08-06 15:15:58 -0700

    redid main.js

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import WebRTC from 'vue-webrtc'
+import * as io from 'socket.io-client'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faTrash, faVideo, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faTrash, faPodcast, faVideo, faUser } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faTrash, faVideo, faUser)
+library.add(faTrash, faPodcast, faVideo, faUser)
+window.io = io
 
 Vue.config.productionTip = false
 Vue.use(WebRTC)
