dbf03b511c985a56b7871d012a3867434c067669
Ray Villalobos <ray@planetoftheweb.com>
2020-08-07 16:50:24 -0700

    fixing interface

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -2,46 +2,56 @@
   <div class="container-fluid mt-4">
     <div class="mb-3">
       <span class="mb-0 h2 text-primary">{{ roomName }}</span>
-      <span class="ml-1">
+      <span class="ml-1" v-if="user !== null && user.uid !== hostID">
         Hosted by: <strong class="text-danger">{{ hostDisplayName }}</strong>
       </span>
     </div>
+
     <div class="row" v-if="(user !== null && user.uid == hostID) || attendeeApproved">
       <div class="col-md-8"></div>
       <div class="col-md-4">
-        <button class="btn btn-primary mr-1">
+        <button
+          v-if="!attendeeJoined && attendeeApproved"
+          class="btn btn-primary mr-1"
+          @click="doJoin"
+        >
           Join
         </button>
-        <button type="button" class="btn btn-danger mr-1">
+        <button v-if="attendeeJoined" type="button" class="btn btn-danger mr-1" @click="doLeave">
           Leave
         </button>
+
         <h4 class="mt-2">Attendees</h4>
         <ul class="list-unstyled">
           <li v-for="attendee in attendeesApproved" :key="attendee.id">
-            <a
-              type="button"
-              class="mr-2"
-              title="Approve attendee"
-              @click="toggleApproval(attendee.id)"
-            >
-              <font-awesome-icon icon="user"></font-awesome-icon>
-            </a>
-
-            <span class="mr-2" title="On Air">
+            <span class="mr-2" v-if="user !== null && user.uid == hostID" title="On Air">
+              <a
+                type="button"
+                class="mr-2"
+                title="Approve attendee"
+                @click="toggleApproval(attendee.id)"
+              >
+                <font-awesome-icon icon="user"></font-awesome-icon>
+              </a>
               <font-awesome-icon icon="podcast"></font-awesome-icon>
             </span>
-            <span></span>
-            <span class="pl-1">{{ attendee.displayName }}</span>
+
+            <span
+              class="pl-1"
+              :class="[attendee.id == user.uid ? 'font-weight-bold text-danger' : '']"
+              >{{ attendee.displayName }}</span
+            >
           </li>
         </ul>
-        <div>
+        <div v-if="user !== null && user.uid == $route.params.hostID">
           <h5 class="mt-4">Pending</h5>
           <ul class="list-unstyled">
             <li class="mb-1" v-for="attendee in attendeesPending" :key="attendee.id">
-              <span>
+              <span v-if="user !== null && user.uid == hostID">
                 <a
                   type="button"
                   class="mr-2"
+                  :class="[attendee.approved ? 'text-secondary' : '', 'text-secondary']"
                   title="Approve attendee"
                   @click="toggleApproval(attendee.id)"
                 >
@@ -56,16 +66,21 @@
                   <font-awesome-icon icon="trash"></font-awesome-icon>
                 </a>
               </span>
-              <span class="pl-1">{{ attendee.displayName }}</span>
+              <span
+                class="pl-1"
+                :class="[attendee.id == user.uid ? 'font-weight-bold text-danger' : '']"
+                >{{ attendee.displayName }}</span
+              >
             </li>
           </ul>
         </div>
       </div>
     </div>
     <div v-else>
-      <p class="lead">
-        Hi <strong class="text-primary font-weight-bold"></strong>, you're currently in the room
-        waiting for the owner of the chat to add you to the meeting. Please wait.
+      <p class="lead" v-if="user">
+        Hi <strong class="text-primary font-weight-bold">{{ user.displayName }}</strong
+        >, you're currently in the room waiting for the owner of the chat to add you to the meeting.
+        Please wait. {{ attendeeApproved }}
       </p>
     </div>
   </div>
@@ -80,6 +95,7 @@ export default {
       attendeesApproved: [],
       attendeesPending: [],
       attendeeApproved: false,
+      attendeeJoined: false,
       hostID: this.$route.params.hostID,
       roomID: this.$route.params.roomID,
       roomName: null,
@@ -135,6 +151,7 @@ export default {
       .doc(this.roomID)
 
     //Get Room Name
+
     roomRef.get().then(roomDocument => {
       if (roomDocument.exists) {
         this.roomName = roomDocument.data().name
@@ -171,7 +188,6 @@ export default {
           if (this.user.uid == attendeeDocument.id) {
             this.attendeeApproved = false
           }
-
           tempPending.push({
             id: attendeeDocument.id,
             displayName: attendeeDocument.data().displayName,
