fd953b5d8fd8c1a40e20427c08e7fead251c934d
Ray Villalobos <ray@planetoftheweb.com>
2020-08-07 17:23:03 -0700

    cleaned up interface

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -2,40 +2,35 @@
   <div class="container-fluid mt-4">
     <div class="mb-3">
       <span class="mb-0 h2 text-primary">{{ roomName }}</span>
-      <span class="ml-1" v-if="user !== null && user.uid !== hostID">
+      <span class="ml-1" v-if="user && user.uid !== hostID">
         Hosted by: <strong class="text-danger">{{ hostDisplayName }}</strong>
       </span>
     </div>
-
     <div class="row" v-if="(user !== null && user.uid == hostID) || attendeeApproved">
       <div class="col-md-8"></div>
       <div class="col-md-4">
-        <button
-          v-if="!attendeeJoined && attendeeApproved"
-          class="btn btn-primary mr-1"
-          @click="doJoin"
-        >
+        <button class="btn btn-primary mr-1">
           Join
         </button>
-        <button v-if="attendeeJoined" type="button" class="btn btn-danger mr-1" @click="doLeave">
+        <button type="button" class="btn btn-danger mr-1">
           Leave
         </button>
-
         <h4 class="mt-2">Attendees</h4>
         <ul class="list-unstyled">
           <li v-for="attendee in attendeesApproved" :key="attendee.id">
-            <span class="mr-2" v-if="user !== null && user.uid == hostID" title="On Air">
-              <a
-                type="button"
-                class="mr-2"
-                title="Approve attendee"
-                @click="toggleApproval(attendee.id)"
-              >
-                <font-awesome-icon icon="user"></font-awesome-icon>
-              </a>
+            <a
+              type="button"
+              class="mr-2"
+              title="Approve attendee"
+              @click="toggleApproval(attendee.id)"
+              v-if="user && user.uid == hostID"
+            >
+              <font-awesome-icon icon="user"></font-awesome-icon>
+            </a>
+
+            <span class="mr-2" title="On Air">
               <font-awesome-icon icon="podcast"></font-awesome-icon>
             </span>
-
             <span
               class="pl-1"
               :class="[attendee.id == user.uid ? 'font-weight-bold text-danger' : '']"
@@ -43,15 +38,14 @@
             >
           </li>
         </ul>
-        <div v-if="user !== null && user.uid == $route.params.hostID">
+        <div v-if="user && user.uid == $route.params.hostID">
           <h5 class="mt-4">Pending</h5>
           <ul class="list-unstyled">
             <li class="mb-1" v-for="attendee in attendeesPending" :key="attendee.id">
-              <span v-if="user !== null && user.uid == hostID">
+              <span>
                 <a
                   type="button"
                   class="mr-2"
-                  :class="[attendee.approved ? 'text-secondary' : '', 'text-secondary']"
                   title="Approve attendee"
                   @click="toggleApproval(attendee.id)"
                 >
@@ -80,7 +74,7 @@
       <p class="lead" v-if="user">
         Hi <strong class="text-primary font-weight-bold">{{ user.displayName }}</strong
         >, you're currently in the room waiting for the owner of the chat to add you to the meeting.
-        Please wait. {{ attendeeApproved }}
+        Please wait.
       </p>
     </div>
   </div>
@@ -95,7 +89,6 @@ export default {
       attendeesApproved: [],
       attendeesPending: [],
       attendeeApproved: false,
-      attendeeJoined: false,
       hostID: this.$route.params.hostID,
       roomID: this.$route.params.roomID,
       roomName: null,
@@ -151,7 +144,6 @@ export default {
       .doc(this.roomID)
 
     //Get Room Name
-
     roomRef.get().then(roomDocument => {
       if (roomDocument.exists) {
         this.roomName = roomDocument.data().name
@@ -188,6 +180,7 @@ export default {
           if (this.user.uid == attendeeDocument.id) {
             this.attendeeApproved = false
           }
+
           tempPending.push({
             id: attendeeDocument.id,
             displayName: attendeeDocument.data().displayName,
